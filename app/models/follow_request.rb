aebfe219b114c0e573232cadb6629266669d23b9
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-18 19:30:48 +0000

    changed sender

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -11,43 +11,41 @@
 #
 
 class FollowRequest < ApplicationRecord
-  validates(:sender, { :presence => true})
+  validates(:sender, { :presence => true })
   validates(:recipient, { :presence => true })
   validates(:recipient_id, {
-    :uniqueness => { :scope => [:sender_id] }
+    :uniqueness => { :scope => [:sender_id] },
   })
 
   # Association accessor methods to define:
-  
+
   ## Direct associations
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
 
   # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
 
-  def sender
-    # my_sender_id = self.sender_id
-    # matching_users = User.where({ :id => my_sender_id })
-    # the_user = matching_users.at(0)
-    # return the_user
+  belongs_to(:sender, 
+              class_name: "User",
+              foreign_key: "sender_id",
+              required: false)
 
-    belongs_to(:sender, 
-      class_name: "User", 
-      foreign_key: "sender_id", 
-      required: true
-    )
-  end
+  belongs_to(:recipient, 
+              class_name: "User",
+              foreign_key: "recipient_id",
+              required: false)
 
-  def recipient
-    # my_recipient_id = self.recipient_id
-    # matching_users = User.where({ :id => my_recipient_id })
-    # the_user = matching_users.at(0)
-    # return the_user
+  # def sender
+  #    my_sender_id = self.sender_id
+  #    matching_users = User.where({ :id => my_sender_id })
+  #    the_user = matching_users.at(0)
+  #    return the_user
+  # end
 
-    belongs_to(:recipient, 
-      class_name: "User",
-      foreign_key: "recipient_id",
-      required: true
-    )
-  end
+  # def recipient
+  #   my_recipient_id = self.recipient_id
+  #   matching_users = User.where({ :id => my_recipient_id })
+  #   the_user = matching_users.at(0)
+  #   return the_user
+  # end
 end
