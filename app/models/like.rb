0ab7c2179994173b9bd58c74d539f0b032f7460c
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-18 19:32:29 +0000

    changed like

--- app/models/like.rb
+++ app/models/like.rb
@@ -24,27 +24,26 @@ class Like < ApplicationRecord
 
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 
-  def fan
-    # my_fan_id = self.fan_id
-    # matching_users = User.where({ :id => my_fan_id })
-    # the_user = matching_users.at(0)
-    # return the_user
+  belongs_to(:fan,
+            class_name: "User",
+            foreign_key: "fan_id",
+            required: false)
+  belongs_to(:photo,
+            class_name: "Photo",
+            foreign_key: "photo_id",
+            required: false)
 
-    #  class_name: "User",
-    #  foreign_key: "fan_id",
-    #  required: true)
-    belongs_to(:fan)
-  end
+  # def fan
+  #    my_fan_id = self.fan_id
+  #    matching_users = User.where({ :id => my_fan_id })
+  #    the_user = matching_users.at(0)
+  #    return the_user
+  # end
 
-  def photo
-    # my_photo_id = self.photo_id
-    # matching_photos = Photo.where({ :id => my_photo_id })
-    # the_photo = matching_photos.at(0)
-    # return the_photo
-
-    #   class_name: "Photo",
-    #   foreign_key: "photo_id",
-    #   required: true
-    belongs_to(:photo)
-  end
+  # def photo
+  #    my_photo_id = self.photo_id
+  #    matching_photos = Photo.where({ :id => my_photo_id })
+  #    the_photo = matching_photos.at(0)
+  #    return the_photo
+  # end
 end
